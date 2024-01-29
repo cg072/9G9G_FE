770668b5fec75cf60215ef47df875c2356de2002
changhyun <cg072@naver.com>
2024-01-29 20:46:23 +0900

    Feat - CalendarViewModel 생성 및 Hilt 적용
    1. CalendarViewModel 생성
    2. Hilt 적용

--- app/src/main/java/com/example/nineg/ui/calendar/CalendarFragment.kt
+++ app/src/main/java/com/example/nineg/ui/calendar/CalendarFragment.kt
@@ -2,11 +2,16 @@ package com.example.nineg.ui.calendar
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.viewModels
 import com.example.nineg.R
 import com.example.nineg.base.BaseFragment
 import com.example.nineg.databinding.FragmentCalendarBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-class CalendarFragment: BaseFragment<FragmentCalendarBinding>() {
+@AndroidEntryPoint
+class CalendarFragment : BaseFragment<FragmentCalendarBinding>() {
+
+    private val viewModel: CalendarViewModel by viewModels()
 
     override val layoutResourceId: Int
         get() = R.layout.fragment_calendar
---
+++ app/src/main/java/com/example/nineg/ui/calendar/CalendarViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.nineg.ui.calendar
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class CalendarViewModel @Inject constructor() : ViewModel() {
+
+}
