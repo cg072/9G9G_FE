f8a63f8a68a5a24c3af17119455e95ac29be28f8
OYJ <oyj7677@gmail.com>
2024-02-12 00:33:40 +0900

    Refactor: MissionCardDto 수정

--- app/src/main/java/com/example/nineg/data/db/dto/MissionCardDto.kt
+++ app/src/main/java/com/example/nineg/data/db/dto/MissionCardDto.kt
@@ -1,26 +1,18 @@
 package com.example.nineg.data.db.dto
 
-import android.os.Parcelable
-import com.google.gson.annotations.SerializedName
-import kotlinx.parcelize.Parcelize
-import java.util.Date
+import com.example.nineg.data.db.entity.MissionCardInfoEntity
 
-@Parcelize
-data class MissionCardDto(
-    @SerializedName("user_id")
-    val userId: String,
-    @SerializedName("title")
-    val title: String,
-    @SerializedName("guide")
-    val guid: String,
-    @SerializedName("content")
-    val content: String,
-    @SerializedName("level")
-    val level: Int,
-    @SerializedName("img")
-    val imgUrl: String,
-    @SerializedName("created_at")
-    val createDate: Date,
-    @SerializedName("updated_at")
-    val updateDate: Date
-) : Parcelable
+class MissionCardDto : ArrayList<MissionCardDtoItem>()
+
+fun MissionCardDto.asDomainModel() : List<MissionCardInfoEntity> {
+    return map {
+        MissionCardInfoEntity(
+            id = it.id,
+            title = it.title,
+            guide = it.guideText,
+            image = it.photoUrl,
+            level = it.level,
+            content = it.content
+        )
+    }
+}
